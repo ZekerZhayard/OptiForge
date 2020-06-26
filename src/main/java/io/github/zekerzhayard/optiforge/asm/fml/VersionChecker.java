486a136054657049962983973de29ecc8dee33d6
ZekerZhayard <trees1000@qq.com>
2020-06-26 20:00:24 +0800

    Compatible with forge 1.15.2-31.2.23. Update OptiFine version checker.

--- src/main/java/io/github/zekerzhayard/optiforge/asm/fml/VersionChecker.java
+++ src/main/java/io/github/zekerzhayard/optiforge/asm/fml/VersionChecker.java
@@ -1,16 +1,30 @@
 package io.github.zekerzhayard.optiforge.asm.fml;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import javax.swing.JOptionPane;
 
+import cpw.mods.modlauncher.api.ITransformationService;
+import net.minecraftforge.fml.loading.LibraryFinder;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.apache.maven.artifact.versioning.VersionRange;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
 
 public class VersionChecker {
     public final static boolean IS_LOADED;
@@ -40,23 +54,14 @@ public class VersionChecker {
         REQUIRED_FORGE_VERSION = properties.getProperty("required.forge.version");
     }
 
-    public static boolean checkOptiFineVersion(Function<String, Boolean> function) {
+    public static boolean checkOptiFineVersion(Function<String, Boolean> function, boolean shouldScan) {
+        StringBuilder mcVersion = new StringBuilder(REQUIRED_MINECRAFT_VERSION);
         try {
             String ofVer = (String) Class.forName("optifine.Installer").getMethod("getOptiFineVersion").invoke(null);
-            Pattern pattern = Pattern.compile("OptiFine_(?<mcVersion>\\d+\\.\\d+\\.\\d*)_HD_U_(?<ofMajorVersion>[A-Z])(?<ofMinorVersion>\\d+)(_pre(?<ofPreVersion>\\d+))?");
-            Matcher matcher = pattern.matcher(ofVer);
-            if (matcher.find()) {
-                String mcVersion = matcher.group("mcVersion");
-                int ofMajorVersion = matcher.group("ofMajorVersion").toCharArray()[0] - 'A' + 1;
-                int ofMinorVersion = Integer.parseInt(matcher.group("ofMinorVersion"));
-                String ofPreVersion = matcher.group("ofPreVersion");
-                if (ofPreVersion == null) {
-                    ofPreVersion = "0";
-                    ofMinorVersion++;
-                }
-                String ofVersion = ofMajorVersion + "." + ofMinorVersion + "." + ofPreVersion;
+            String ofVersion = parseOptiFineVersion(ofVer, mcVersion);
+            if (ofVersion != null) {
                 VersionRange versionRange = VersionRange.createFromVersionSpec("[" + REQUIRED_OPTIFINE_VERSION + ",)");
-                if (!mcVersion.equals(REQUIRED_MINECRAFT_VERSION) || !versionRange.containsVersion(new DefaultArtifactVersion(ofVersion))) {
+                if (!mcVersion.toString().equals(REQUIRED_MINECRAFT_VERSION) || !versionRange.containsVersion(new DefaultArtifactVersion(ofVersion))) {
                     DefaultArtifactVersion requiredOFVersion = new DefaultArtifactVersion(REQUIRED_OPTIFINE_VERSION);
                     String requiredOptiFineCompletedVersion = "OptiFine_" + REQUIRED_MINECRAFT_VERSION + "_HD_U_" + (char) (requiredOFVersion.getMajorVersion() + 'A' - 1) + (requiredOFVersion.getMinorVersion() + (requiredOFVersion.getIncrementalVersion() == 0 ? -1 : 0)) + (requiredOFVersion.getIncrementalVersion() == 0 ? "" : "_pre" + requiredOFVersion.getIncrementalVersion());
                     return function.apply(
@@ -73,6 +78,46 @@ public class VersionChecker {
                 );
             }
         } catch (Exception e) {
+            // Some people always put the "OptiFine" jar which from ".minecraft/versions/<OptiFine-Version>" folder into the mods folder, but that is not real OptiFine jar.
+            // Here is to scan ".minecraft/libraries/optifine/OptiFine" folder to find the real OptiFine.
+            if (shouldScan) {
+                try {
+                    Method method = LibraryFinder.class.getDeclaredMethod("findLibsPath");
+                    method.setAccessible(true);
+                    Path libOptiFinePath = ((Path) method.invoke(null)).resolve("optifine").resolve("OptiFine");
+                    if (Files.isDirectory(libOptiFinePath)) {
+                        String optifineVersion = "";
+                        Path libOptiFine = null;
+                        for (Path p : Files.list(libOptiFinePath)
+                            .filter(p -> Files.isRegularFile(p.resolve("OptiFine-" + p.getFileName().toString() + ".jar")) && p.getFileName().toString().split("_")[0].equals(mcVersion.toString()))
+                            .map(p -> p.resolve("OptiFine-" + p.getFileName().toString() + ".jar")).collect(Collectors.toList())) {
+
+                            LOGGER.info("Found a possible OptiFine jar: {}", p.toAbsolutePath());
+                            String ofVersion = parseOptiFineVersion(getOptiFineVersionFromJar(p), null);
+                            if (ofVersion != null && optifineVersion.equals("")) {
+                                optifineVersion = ofVersion;
+                            }
+                            if (ofVersion != null && new DefaultArtifactVersion(ofVersion).compareTo(new DefaultArtifactVersion(optifineVersion)) > 0) {
+                                optifineVersion = ofVersion;
+                                libOptiFine = p;
+                            }
+                        }
+
+                        if (libOptiFine != null) {
+                            LOGGER.info("Found the real OptiFine jar: {}", libOptiFine.toAbsolutePath());
+                            MethodUtils.invokeMethod(VersionChecker.class.getClassLoader(), true, "addURL", libOptiFine.toUri().toURL());
+
+                            // Add OptiFinTransformerService.
+                            (FakeOptiFineTransformationService.getInstance().service = (ITransformationService) Class.forName("optifine.OptiFineTransformationService").newInstance()).onLoad(FakeOptiFineTransformationService.getInstance().getEnv(), FakeOptiFineTransformationService.getInstance().getOtherServices());
+
+                            return checkOptiFineVersion(function, false);
+                        }
+                    }
+                } catch (Throwable throwable) {
+                    LOGGER.error("", throwable);
+                }
+            }
+
             StringBuilder message = new StringBuilder(
                 "It looks like you have not put OptiFine itself into the mods folder, you can download it from https://optifine.net/downloads.\n" +
                 "The game will continue, and run without OptiFine and OptiForge.\n\n" +
@@ -85,6 +130,55 @@ public class VersionChecker {
             return function.apply(message.toString());
         }
         return true;
+    }
+
+    /**
+     * Parse OptiFine version.
+     *
+     * Input: OptiFine_1.0.0_HD_U_A1_pre1
+     * Output: return: 1.1.1, mcVersion: 1.0.0
+     *
+     * Input: OptiFine_1.0.0_HD_U_A1
+     * Output: return: 1.2.0, mcVersion: 1.0.0
+     */
+    public static String parseOptiFineVersion(String optifineVersion, StringBuilder mcVersion) {
+        Pattern pattern = Pattern.compile("OptiFine_(?<mcVersion>\\d+\\.\\d+\\.\\d*)_HD_U_(?<ofMajorVersion>[A-Z])(?<ofMinorVersion>\\d+)(_pre(?<ofPreVersion>\\d+))?");
+        Matcher matcher = pattern.matcher(optifineVersion);
+        if (matcher.find()) {
+            if (mcVersion != null) {
+                mcVersion.delete(0, mcVersion.length());
+                mcVersion.append(matcher.group("mcVersion"));
+            }
+            int ofMajorVersion = matcher.group("ofMajorVersion").toCharArray()[0] - 'A' + 1;
+            int ofMinorVersion = Integer.parseInt(matcher.group("ofMinorVersion"));
+            String ofPreVersion = matcher.group("ofPreVersion");
+            if (ofPreVersion == null) {
+                ofPreVersion = "0";
+                ofMinorVersion++;
+            }
+            return ofMajorVersion + "." + ofMinorVersion + "." + ofPreVersion;
+        }
+        return null;
+    }
+
+    public static String getOptiFineVersionFromJar(Path optifine) {
+        try (ZipFile zf = new ZipFile(optifine.toFile())) {
+            ZipEntry ze = zf.getEntry("net/optifine/Config.class");
+            if (ze != null) {
+                try (InputStream is = zf.getInputStream(ze)) {
+                    ClassNode cn = new ClassNode();
+                    new ClassReader(IOUtils.toByteArray(is)).accept(cn, ClassReader.EXPAND_FRAMES);
+                    for (FieldNode fn : cn.fields) {
+                        if (fn.name.equals("VERSION") && fn.desc.equals("Ljava/lang/String;")) {
+                            return (String) fn.value;
+                        }
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            LOGGER.catching(t);
+        }
+        return null;
     }
 
     /**
